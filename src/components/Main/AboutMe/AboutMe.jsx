8bb3c485a3efebd2c94cc3e02a28fde6653c485b
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-08-16 19:30:05 -0400

    Summary

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import "./AboutMe.css";
+import { Link } from "react-router-dom";
+import student from "../../../images/student.jpg";
 
-function AboutMe (){
-  return console.log ('hi me');
+function AboutMe() {
+  return (
+    <section className="about-me">
+      <h2 className="about-me__title section__title">Студент</h2>
+      <div className="about-me__student-info">
+        <div className="about-me__text-content">
+          <h3 className="about-me__text-content_subtitle section__subtitle">
+            {" "}
+            Виталий
+          </h3>
+          <p className="about-me__text-content_student-major">
+            Фронтенд-разработчик, 30 лет
+          </p>
+          <p className="about-me__text-content_caption section__caption section__caption">
+            Я родился и живу в Саратове, закончил факультет экономики СГУ. У
+            меня есть жена и дочь. Я люблю слушать музыку, а ещt увлекаюсь
+            бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ
+            Контур». После того, как прошёл курс по веб-разработке, начал
+            заниматься фриланс-заказами и ушёл с постоянной работы.
+          </p>
+        </div>
+        <img className="about-me__student_photo" alt="student" src={student} />
+      </div>
+      <Link
+        className="about-me__link-github"
+        type="link"
+        to="https://github.com/Kelendis2"
+      >
+        Github
+      </Link>
+    </section>
+  );
 }
 
 export default AboutMe;
--- src/components/Main/Portfolio /Portfolio.jsx
+++ src/components/Main/Portfolio /Portfolio.jsx
@@ -1,6 +1,50 @@
 import React from "react";
+import "./Portfolio.css"
+import strelka from "../../../images/strelka.svg";
+import { Link } from "react-router-dom";
 function Portfolio (){
-  return console.log ('hi Portfolio');
+  return (
+    <section className="portfolio">
+    <h4 className="portfolio__title">Портфолио</h4>
+    <Link
+      className="portfolio__link"
+      type="link"
+      to="https://github.com/Kelendis2/how-to-learn"
+    >
+      <p className="portfolio-link__text">Статичный сайт</p>
+      <img
+        className="portfolio-link__image"
+        alt="strelka"
+        src={strelka}
+      />
+    </Link>
+    <Link
+      className="portfolio__link"
+      type="link"
+      to="https://github.com/Kelendis2/russian-travel"
+    >
+      <p className="portfolio-link__text">Адаптивный сайт</p>
+
+      <img
+        className="about-me__portfolio-link_image"
+        alt="strelka"
+        src={strelka}
+      />
+    </Link>
+    <Link
+      className="portfolio__link"
+      type="link"
+      to="https://github.com/Kelendis2/react-mesto-api-full-gha"
+    >
+      <p className="portfolio-link__text">Одностраничное приложение</p>
+      <img
+        className="about-me__portfolio-link_image"
+        alt="strelka"
+        src={strelka}
+      />
+    </Link>
+  </section>
+  )
 }
 
 export default Portfolio;
