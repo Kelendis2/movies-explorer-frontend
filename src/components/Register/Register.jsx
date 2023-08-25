5793a67d398812f3837adf22de9680d14328d51d
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-08-24 21:29:44 -0400

    all Mobile

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -7,13 +7,22 @@ function Register() {
     <section className="register">
       <form className="register__form form">
         <label className="register__form_label form_label"> Имя </label>
-        <input className="register__form_input form_input"></input>
+        <input
+          className="register__form_input form_input"
+          placeholder="Виталий"
+        ></input>
 
         <label className="register__form_label form_label">E-mail</label>
-        <input className="register__form_input form_input"></input>
+        <input
+          className="register__form_input form_input"
+          placeholder="pochta@yandex.ru"
+        ></input>
 
         <label className="register__form_label form_label">Пароль</label>
-        <input className="register__form_input form_input"></input>
+        <input
+          className="register__form_input form_input"
+          placeholder="••••••••••••••"
+        ></input>
 
         <span className="register__form_span-error form_span-error"></span>
         <button className="register__form_button form_button" type="button">
