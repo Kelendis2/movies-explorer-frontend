cedd5ac7d90782aa38cf4c0c1961790acc512473
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-08-29 13:16:57 -0400

    portfolio fix name

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import AboutMe from "./AboutMe/AboutMe";
 import AboutProject from "./AboutProject/AboutProject";
-import Portfolio from "./Portfolio /Portfolio";
+import Portfolio from "./Portfolio/Portfolio";
 import Promo from "./Promo/Promo";
 import Techs from "./Techs/Techs";
 
